2c7ac6ae0b2ac5847384cf04ef617ec38d7dc7e3
alexanderagreda <alexanderagreda@gmail.com>
2018-09-09 21:23:15 -0500

    start project

---
+++ materialize/index.php
@@ -0,0 +1,6 @@
+<?php
+$ins = $con->query("INSERT INTO mitabla VALUES('') ");
+$del = $con->query("DELETE FROM mitabla FROM id='' ");
+$up = $con->query("UPDATE mitabla SET micampo ='' WHERE id='' ");
+$sel = $con->query("SELECT * FROM mitabla ");
+?>
