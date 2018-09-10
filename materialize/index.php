a2c213ee393e7adca47aff25290a704351a9ec2c
alexander <alexanderagreda@gmail.com>
2018-09-10 18:13:45 -0500

    sidenav

--- materialize/index.php
+++
@@ -1,1 +0,0 @@
-<?php $ins = $con->query("INSERT INTO mitabla VALUES('') "); $sel = $con->query("SELECT * FROM mitabla "); $del = $con->query("DELETE FROM mitabla FROM id='' "); $up = $con->query("UPDATE mitabla SET micampo ='' WHERE id='' ");?>
