09bc7dee69c400a84b348aad15432dcc777e1d6f
alexanderagreda <alexanderagreda@gmail.com>
2018-09-12 21:35:02 -0500

    end basic structure

---
+++ proyecto/extend/header.php
@@ -0,0 +1,18 @@
+<?php include '../conexion/conexion.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scanle=1.0">
+    <meta http-equiv="X-UA-Compatible" content-type="ie-edge">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link rel="stylesheet" href="../css/materialize.min.css">
+    <!--Import sweetalert2.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.26.28/sweetalert2.css">
+    <title></title>
+  </head>
+  <body>
+    <main>
+  
---
+++ proyecto/extend/scripts.php
@@ -0,0 +1,7 @@
+  </main>
+  <!--1. Import jQuery-->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <!--2. Import materialize.js-->
+  <script type="text/javascript" src="../js/materialize.min.js"></script>
+  <!--3. Import sweetalert2.js-->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.26.28/sweetalert2.js"></script>
