a76d4847911c3aa8c035d028510caf32954e69ea
alexander <alexanderagreda@gmail.com>
2018-09-26 18:01:30 -0500

    add validation conexion

--- proyecto/conexion/conexion.php
+++ proyecto/conexion/conexion.php
@@ -1,5 +1,12 @@
 <?php
 
   $con = new mysqli('localhost','root','mysql','inmobiliaria');
+  //Validar errores de conexion
+  if ($con->connect_errno) {
+    echo "Error: Fallo al conectarse a la base de datos debido a: <br />";
+    echo "Errno: " . $con->connect_errno . "<br />";
+    echo "Error: " . $con->connect_error . "<br />";
+    exit;
+  }
 
 ?>
