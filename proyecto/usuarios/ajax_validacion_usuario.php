68e9a495b739a29c19cbbce301a948db17a64f15
alexanderagreda <alexanderagreda@gmail.com>
2018-09-25 22:30:50 -0500

    contar filas del resultado insertar usuarios

--- proyecto/usuarios/ajax_validacion_usuario.php
+++ proyecto/usuarios/ajax_validacion_usuario.php
@@ -3,6 +3,7 @@
     $usuario = $con->real_escape_string($_POST['usuario']); //escapar caracteres especiales
     $sel = $con->query("SELECT id FROM inmobiliaria.usuario WHERE usuario = '$usuario' "); //ejecutar consulta
     if($sel){
+      $row = $sel->num_rows;
       if ($row != 0 ) {
         echo "<label style='color:red;'>El nombre de usuario ya existe</label>";
       }else{
